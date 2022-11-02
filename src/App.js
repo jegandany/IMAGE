79906a509d51130eca7b2662ab32993ceb0ddcd4
unknown <jeganvedha00@gmail.com>
2022-11-02 12:04:25 +0530

    inition commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import car from './imgs/image2.jpg'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <img src={car} /> 
+    </div>
+
+  );
+}
+
+export default App;
